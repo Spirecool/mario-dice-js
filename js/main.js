0c13344e64a316bdce5b95feca88cb580ee9ef45
Jérôme OLLIVIER <54033192+Spirecool@users.noreply.github.com>
2022-03-01 18:53:08 +0100

    JS : function who win

--- js/main.js
+++ js/main.js
@@ -1,45 +1,51 @@
+
 // *********************************
 // *** Déclaration des variables ***
 // *********************************
 
 let scores, roundScore, gamePlay, activePlayer, playTill, winningScore;
 
+
 // ****************************************
 // *** Déclaration des variables audios ***
 // ****************************************
 
-const audioBattle = new Audio("audio/battle-sound.wav"); // son de la victoire
-const audioWin = new Audio("audio/victory-sound.wav"); // son de la victoire
-const audioDiceShaking = new Audio("audio/dice-roll-sound.mp3"); // son au lancer du dé
-const audioLost = new Audio("audio/lost-sound.wav");
-const audioHold = new Audio("audio/hold-sound.wav");
-const audioNewGame = new Audio("audio/new-game-sound.wav");
+const audioBattle = new Audio('audio/battle-sound.wav'); // son de la victoire
+const audioWin = new Audio('audio/victory-sound.wav'); // son de la victoire
+const audioDiceShaking = new Audio('audio/dice-roll-sound.mp3'); // son au lancer du dé
+const audioLost = new Audio ('audio/lost-sound.wav');
+const audioHold = new Audio ('audio/hold-sound.wav');
+const audioNewGame = new Audio ('audio/new-game-sound.wav');
 const audio = [audioWin, audioDiceShaking, audioLost, audioNewGame];
 
 // Déclaration des constantes du DOM pour le bouton MUTE
-const muted = document.getElementById("mute");
-const mutedText = document.getElementById("mute-text");
+const muted = document.getElementById('mute');
+const mutedText = document.getElementById('mute-text');
+
 
 // ****************************************
-// *** Fonction qui initialise le jeu ******
+// *** Fonction qui initalise le jeu ******
 // ****************************************
+
 
 init();
-document.querySelector(".dice").style.display = "block"; // affiche le dé au lancement du jeu
+document.querySelector('.dice').style.display = 'block';  // affiche le dé au lancement du jeu
+
 
 
 /**********************************/
 /***Choisir le nom des joueurs ***/
 /*********************************/
 
-document.querySelector(".btn-enter").addEventListener("click", function () {
-  document.querySelector(".player-names").style.display = "none";
+
+document.querySelector('.btn-enter').addEventListener('click', function() {
+  document.querySelector('.player-names').style.display = 'none';
   var player1Name = document.getElementById("player-name-1").value;
   var player2Name = document.getElementById("player-name-2").value;
-  document.querySelector("#name-0").textContent = player1Name;
-  document.querySelector("#name-1").textContent = player2Name;
-});
+  document.querySelector('#name-0').textContent = player1Name;
+  document.querySelector('#name-1').textContent = player2Name;
 
+});
 
 
 
@@ -75,17 +81,48 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 });
 
 
-
 // *********************************
 // *** Fonction sécuriser (hold) ***
 // *********************************
 
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
-    if (gamePlay) { // si le jeu est en cours
-        scores[activePlayer] += roundScore; // ajoute le score courant (ROUND) au score global
-        audioHold.play();
-  
-        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-  
-  
+  if (gamePlay) { // si le jeu est en cours
+      scores[activePlayer] += roundScore; // ajoute le score courant (ROUND) au score global
+      audioHold.play();
+
+      document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+
+// ******************************
+// *** On vérifie qui a gagné ***
+// ******************************
+
+// On vérifie si le joueur a gagné la partie 
+playTill = document.querySelector(".play-till").value;
+
+document.querySelector("#score-" + activePlayer).textContent =
+    scores[activePlayer];
+  //check if player won game
+  let input = document.querySelector(".play-till").value;
+
+  if (input) {
+    winningScore = input;
+  } else {
+    winningScore = 100;
+  }
+      if (scores[activePlayer] >= winningScore) {
+          document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+          audioWin.play();
+          document.querySelector('.dice').style.display = 'block';
+          //on supprime la classe winner du block du joueur actif
+          document.querySelector(("player-name-1").value + activePlayer + '-panel').classList.add('winner');
+          // document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner'); // sinon ce code
+          document.querySelector(("player-name-2").value  + activePlayer + '-panel').classList.remove('active');
+          gamePlay = false; // on enlève la possibilité de continuer la partie (roll dice et hold ne lancent plus rien)
+      } else {
+          nextPlayer();
+      }
+  }
+});
+
